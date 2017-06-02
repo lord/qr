fe6f02163296cb63f4bfeeb97185e9ae4e2dfc04
Robert Lord <robert@lord.io>
2017-06-02 15:33:32 -0500

    Add reserved areas

--- qr.py
+++ qr.py
@@ -4,14 +4,10 @@ class Color(Enum):
   black = 2
   unset = 3
 
-class Type(Enum):
-  functional = 1
-  data = 2
-
 class Module:
   def __init__(self):
     self.color = Color.unset
-    self.type = Type.data
+    self.reserved = False
 
 def display_qr(data):
   for row in data:
@@ -20,8 +16,10 @@ def display_qr(data):
         print("\033[40m  \033[0m", end="")
       elif cell.color == Color.white:
         print("\033[47m  \033[0m", end="")
+      elif cell.reserved == True:
+        print("\033[42m  \033[0m", end="") # unset but reserved space
       else:
-        print("\033[41m  \033[0m", end="") # draw as red
+        print("\033[41m  \033[0m", end="") # completely unset space
     print()
 
 def finder_pattern(data, xstart, ystart):
@@ -29,7 +27,7 @@ def finder_pattern(data, xstart, ystart):
     for y in range(ystart, ystart+9):
       if x >= 0 and y >= 0 and x < len(data) and y < len(data[0]): # don't draw if off side of code
         from_center = max(abs(x-xstart-4), abs(y-ystart-4))
-        data[y][x].type = Type.functional
+        data[y][x].reserved = True
         if from_center == 2 or from_center == 4:
           data[y][x].color = Color.white
         else:
@@ -39,12 +37,12 @@ def alignment_pattern(data, xstart, ystart):
   # check to make sure the space isn't occupied by something already
   for x in range(xstart, xstart+5):
     for y in range(ystart, ystart+5):
-      if data[y][x].type == Type.functional:
+      if data[y][x].reserved == True:
         return
 
   for x in range(xstart, xstart+5):
     for y in range(ystart, ystart+5):
-      data[y][x].type = Type.functional
+      data[y][x].reserved = True
       if max(abs(x-xstart-2), abs(y-ystart-2)) == 1:
         data[y][x].color = Color.white
       else:
@@ -83,17 +81,31 @@ def base(version):
 
   # add timing patterns
   for i in range(size):
-    if data[i][6].type == Type.data:
-      data[i][6].type = Type.functional
+    if data[i][6].reserved == False:
+      data[i][6].reserved = True
       data[i][6].color = Color.black if i % 2 == 0 else Color.white
-    if data[6][i].type == Type.data:
-      data[6][i].type = Type.functional
+    if data[6][i].reserved == False:
+      data[6][i].reserved = True
       data[6][i].color = Color.black if i % 2 == 0 else Color.white
 
   # add dark module
   data[size-8][8].color = Color.black
-  data[size-8][8].type = Type.functional
-  # TODO add reserved areas
+  data[size-8][8].reserved = True
+
+  # add reserved areas
+  if version < 7:
+    data[8][8].reserved = True
+    for i in range(0, 8):
+      data[i][8].reserved = True
+      data[8][i].reserved = True
+      data[8][size-i-1].reserved = True
+      data[size-i-1][8].reserved = True
+  else:
+    for i in range(0, 3):
+      for j in range(0, 6):
+        data[size-9-i][j].reserved = True
+        data[j][size-9-i].reserved = True
+
   return data
 
 import sys
